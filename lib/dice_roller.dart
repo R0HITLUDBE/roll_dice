28adb703112d7ad9a1934328208780e382455cf6
ROHIT LUDBE <rohitludbe@kccemsr.edu.in>
2023-07-30 19:30:22 +0530

    completed roll dice

---
+++ lib/dice_roller.dart
@@ -0,0 +1,39 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+var randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 1;
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+            ),
+            child: const StyledText('Roll Dice'))
+      ],
+    );
+  }
+}
